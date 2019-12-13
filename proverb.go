4001d85600a455764389943eef4439e47403fbf9
shane <shanecandoit@gmail.com>
2019-12-13 09:59:21 -0600

    tests pass. add final line.

--- proverb.go
+++ proverb.go
@@ -1,29 +1,43 @@
-// Package proverb match an input string to its proverb
+// Package proverb create a proverb that uses the given strings. Pairs from a list.
 package proverb
+
+import "strings"
 
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
 	/*
-		Given a list of inputs, generate the relevant proverb. For example, given the list
-		["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"], you will output the full text of this proverbial rhyme:
+	Given a list of inputs: ["nail", "shoe", "horse", ...]
+	output the full text of this proverbial rhyme:
 
-		For want of a nail the shoe was lost.
-		For want of a shoe the horse was lost.
-		For want of a horse the rider was lost.
-		For want of a rider the message was lost.
-		For want of a message the battle was lost.
-		For want of a battle the kingdom was lost.
-		And all for the want of a nail.
-
-		For want of a s[0] the s[1] was lost.
-		For want of a s[1] the s[2] was lost.
+		For want of a $1 the $2 was lost.
+		For want of a $2 the $3 was lost.
+		...
+		And all for the want of a $1.
 	*/
 
-	head, mid, tail := "For want of a", "the", "was lost."
+	line := "For want of a $1 the $2 was lost."
+	last := "And all for the want of a $1."
 
 	var result []string
 
-	result = append(result, head, "nail", mid, "shoe", tail)
+	// empty input, empty output
+	sz := len(rhyme)
+	if sz == 0 {
+		return result
+	}
+
+	for i, str := range rhyme {
+		// and grab the next str too
+		if i < sz-1 {
+			str2 := rhyme[i+1]
+			line1 := strings.ReplaceAll(line, "$1", str)
+			line2 := strings.ReplaceAll(line1, "$2", str2)
+			result = append(result, line2)
+		}
+	}
+
+	// end with last line. "And all for the want of a $1." rhyme[0]
+	result = append(result, strings.Replace(last, "$1", rhyme[0], 1))
 
 	return result
 }
